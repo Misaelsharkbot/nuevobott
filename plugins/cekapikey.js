351af22866990a74f37dd1a002f9eab5eb154a41
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-14 16:46:53 -0500

    Update cekapikey.js

--- plugins/cekapikey.js
+++ plugins/cekapikey.js
@@ -1,8 +1,8 @@
 let get = require('node-fetch')
 let handler = async (m, { conn,text,args}) => {
  let data = await get('https://zerochanbot.herokuapp.com/api/cekapikey?apikey='+args[0]).then(v => v.json())
-  if (data.status != 200) throw `API : https://zerochanbot.herokuapp.com\nApikey : ${args[0]}\n\nApikey Invalid, Silahkan Chat Owner Untuk Membeli Apikey`
-  if (data.status == 200) m.reply(`API : https://zerochanbot.herokuapp.com\nApikey : ${args[0]}\nStatus : Valid\n\nApikey Kamu Valid, Cuma Berlaku 30 hari Doang Ya Kak:)`)
+  if (data.status != 200) throw `API : https://zerochanbot.herokuapp.com\nApikey : ${args[0]}\n\nApikey Inválido, chatee con el propietario para comprar apikey`
+  if (data.status == 200) m.reply(`API : https://zerochanbot.herokuapp.com\nApikey : ${args[0]}\nStatus : Válido\n\nTu Apikey es Válido, Sólo válido por 30 días, hermano:)`)
 }
 handler.help = ['cekey']
 handler.tags = ['api']
