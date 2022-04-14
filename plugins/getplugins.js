fc65563ea73eee3297caf64bd514bc081129bde7
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-14 18:05:03 -0500

    Update getplugins.js

--- plugins/getplugins.js
+++ plugins/getplugins.js
@@ -1,8 +1,8 @@
 let handler = async (m, { usedPrefix, command, text }) => {
     let ar = Object.keys(plugins)
     let ar1 = ar.map(v => v.replace('.js', ''))
-    if (!text) throw `uhm.. teksnya mana?\n\ncontoh:\n${usedPrefix + command} sc`
-    if (!ar1.includes(text)) return m.reply(`'${text}' tidak ditemukan!\n\n${ar1.map(v => ' ' + v).join`\n`}`)
+    if (!text) throw `uhm.. ¿dónde está el texto?\n\nEjemplo:\n${usedPrefix + command} sc`
+    if (!ar1.includes(text)) return m.reply(`'${text}' ¡extraviado!\n\n${ar1.map(v => ' ' + v).join`\n`}`)
     m.reply(require('fs').readFileSync('./plugins/' + text + '.js', 'utf-8'))
 }
 handler.help = ['getplugin'].map(v => v + ' <teks>')
