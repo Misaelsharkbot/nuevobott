bc4125355ef794759f60d474273116a35c02dc87
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-14 18:03:30 -0500

    Update gethentai.js

--- plugins/gethentai.js
+++ plugins/gethentai.js
@@ -7,9 +7,9 @@ let nhentai = require('nhentai-node-api')
 let handler = async (m, { conn, args }) => {
 	if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys) return !0
 	if (m.quoted && m.quoted.fromMe && m.quoted.isBaileys && /getnhentai 1/i.test(m.quoted.text)) {
-		if (!args[0]) return m.reply('Masukkan angka')
-		if (isNaN(args[0])) return m.reply('Pake angka')
-		await m.reply('Loading...')
+		if (!args[0]) return m.reply('Introduce números')
+		if (isNaN(args[0])) return m.reply('Usar números')
+		await m.reply('Cargando...')
 		let data = JSON.stringify(await eval(`${args[0]}-1`))
 		let input = m.quoted.text.split('='.repeat(25))[data].split('ID: ')[1].split('\n')[0]
 		let count = 0
