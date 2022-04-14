6026e6be05a24e941833a4ff564665ea74f001bd
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-14 15:40:49 -0500

    Update banuser.js

--- plugins/banuser.js
+++ plugins/banuser.js
@@ -2,14 +2,14 @@ let handler = async (m, { conn, args }) => {
     if (!args || !args[0]) throw 'Siapa yang mau di banned om?'
     let mention = m.mentionedJid[0] || conn.parseMention(args[0]) || (args[0].replace(/[@.+-]/g, '').replace(' ', '') + '@s.whatsapp.net') || ''
     if (!mention) throw 'Tag salah satu lah'
-    if (!(mention in global.DATABASE._data.users)) throw 'User tidak terdaftar dalam DATABASE!!'
+    if (!(mention in global.DATABASE._data.users)) throw '¡¡El usuario no está registrado en la BASE DE DATOS!!'
     let user = global.DATABASE._data.users[mention]
-    if (user.Banneduser) throw 'User telah terbanned!!'
+    if (user.Banneduser) throw '*¡El usuario ha sido baneado!*'
     let txt = (args.length > 1 ? args.slice(1).join(' ') : '') || '' 
     user.Banneduser = true 
     user.BannedReason = txt 
-    m.reply('Berhasil Banned USER!')
-    m.reply('*Kamu dibanned oleh OWNER Atau MODERATOR!!*\n *HUBUNGI* \n' + global.owner.map((v, i) => '*Owner ' + (i + 1) + ':* wa.me/' + v).join('\n') + '\n\n' + global.mods.map((v, i) => '*Moderator ' + (i + 1) + ':* wa.me/' + v).join('\n'), mention)
+    m.reply('¡USUARIO baneado exitosamente!')
+    m.reply('*Estas baneado por el PRPIETARIO o MODERADOR!*\n *CONTACTO* \n' + global.owner.map((v, i) => '*Owner ' + (i + 1) + ':* wa.me/' + v).join('\n') + '\n\n' + global.mods.map((v, i) => '*MODERADOR ' + (i + 1) + ':* wa.me/' + v).join('\n'), mention)
 }
 
 handler.help = ['ban']
