ccdd723d72019f511edf68d0f9573eacb136858b
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-14 15:45:01 -0500

    Update bass.js

--- plugins/bass.js
+++ plugins/bass.js
@@ -22,7 +22,7 @@ let handler = async (m, { conn }) => {
 						conn.sendFile(m.chat, buff, ran, null, m, true)
 						fs.unlinkSync(ran)
             })
-        } else m.reply('Kirim audio atau tag audio!!')
+        } else m.reply('Enviar un audio o etiqueta un audio!!')
     } catch (e) {
         console.log(e)
         m.reply('Error! !')
