3ec58f91e1f7e2aeb67584523a1bff5c7d074b24
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-14 18:01:58 -0500

    Update getcode.js

--- plugins/getcode.js
+++ plugins/getcode.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, usedPrefix }) => {
-  if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, 'Command ini hanya untuk yang jadi bot', m)
+  if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, 'Este comando es solo para bots', m)
   else global.conn.reply(conn.user.jid, `${usedPrefix}jadibot ${Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString('base64')}`, m)
 }
 handler.help = ['getcode']
 handler.tags = ['jadibot']
 handler.command = /^(getcode)$/i
-handler.owner = true
-handler.mods = false
+handler.owner = false
+handler.mods = true
 handler.premium = false
 handler.group = false
 handler.private = false
