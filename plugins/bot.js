369db95c62af05194b3d5f9289ad2d0cd68e2d7d
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-14 15:49:55 -0500

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn, text, participants }) => {
-conn.reply(m.chat, `Iya Kak? ${conn.getName(conn.user.jid)} Online Ketik Aja *#menu* Untuk Melihat List Menu *${conn.getName(conn.user.jid)}* :)\n\nSilahkan Ketik #menu`, 'conversation', { quoted: m, contextInfo: { externalAdReply :{
-mediaUrl: 'https://wa.me/62895603352610?text=Hai.com',
+conn.reply(m.chat, `¿Si hermano? ${conn.getName(conn.user.jid)} En línea Solo escriba *#menu* Para ver la lista del menú *${conn.getName(conn.user.jid)}* :)\n\nPor favor escribe #menu`, 'conversacion', { quoted: m, contextInfo: { externalAdReply :{
+mediaUrl: 'https://wa.me/51977783315?text=Hai.com',
 mediaType: 2,
-title: `Simple Bot Whatsapp`,
+título: `Simple Bot Whatsapp`,
 body: '© YUKIBOT',
 thumbnailUrl: 'https://i.ibb.co/TYBsTsV/1890eb474e10.jpg',
 }}}) 
