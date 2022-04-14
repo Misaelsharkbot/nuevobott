32431e9d160a0b7fd00e69bf13ab442f1ae2621c
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-14 18:11:16 -0500

    Update glitch.js

--- plugins/glitch.js
+++ plugins/glitch.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args, text }) => {
    let [t1, t2] = text.split `|`
-  if (!args[0]) throw 'Masukkan Parameter'
-  m.reply('Sedang Diproses...')
+  if (!args[0]) throw 'Introducir parámetros'
+  m.reply('Siendo procesado...')
   let res = await fetch(`https://leyscoders-api.herokuapp.com/api/textmaker/glitch?q1=${t1}&q2=${t2}&apikey=MIMINGANZ`)
   let json = await res.json()
   conn.sendFile(m.chat, json.result.url, 'glitch.jpg', `Nih kak`, m, false)
