b38cec60c159b2a20b2eff7d8fb5eab004dc172f
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-14 16:35:02 -0500

    Update call.js

--- plugins/call.js
+++ plugins/call.js
@@ -1,12 +1,12 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, text, usedPrefix }) => {
 
-if (!text) throw `Contoh Penggunaan\n${usedPrefix}call 628xxxxxxxx`
+if (!text) throw `Ejemplo de uso\n${usedPrefix}call 51977783xxx`
 let nomor = text.replace(/[^0-9]/gi, '').slice(2)
-if (!nomor.startsWith('8')) throw `Contoh Penggunaan\n${usedPrefix}call 628xxxxxxxx`
+if (!nomor.startsWith('8')) throw `Ejemplo de uso\n${usedPrefix}call 51977783xxx`
 m.reply('*_Tunggu permintaan anda sedang diproses_*')
 let anu = await fetch(`https://id.jagreward.com/member/verify-mobile/${nomor}`).then(a => a.json())
-let spcall = `Sukses mengirimkan call ke 62${nomor}`
+let spcall = `Envió con éxito una llamada a 62${nomor}`
 conn.reply(m.chat, `${spcall}`.trim(), m)
 m.reply(anu)
 }
