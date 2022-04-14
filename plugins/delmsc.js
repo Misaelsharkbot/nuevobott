8d3c4c4393837000e5200ad5537f6adb71d633fe
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-14 17:29:09 -0500

    Update delmsc.js

--- plugins/delmsc.js
+++ plugins/delmsc.js
@@ -1,7 +1,7 @@
 let limit = 30
 const { servers, yta } = require('../lib/y2mate')
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
+  if (!args || !args[0]) throw 'Uhm... ¿dónde está la URL??'
   let server = (args[1] || 'id4').toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF} = await yta(args[0], servers.includes(server) ? server : 'id4')
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
@@ -9,8 +9,8 @@ let handler = async (m, { conn, args, isPrems, isOwner }) => {
  let y = fs.readFileSync('./logo.jpg')
 
 
-conn.sendMessage(m.chat, `*Title:* ${title}\n*Size:* ${filesizeF}` , 'conversation', {quoted: m, thumbnail: global.thumb2, contextInfo:{externalAdReply: {title: 'Simple WhatsApp bot', body: `© ${conn.user.name}`, sourceUrl: '', thumbnail: y}}})
-  if (!isLimit) conn.sendFile(m.chat, dl_link , 'conversation', {quoted: m, thumbnail: y, contextInfo:{externalAdReply: {title: `${title}`, body: `© ${conn.user.name}`, sourceUrl: '', thumbnail: y}}}, m, false, {ptt: true, duration: 999999999999})
+conn.sendMessage(m.chat, `*Titulo:* ${title}\n*Tamaño:* ${filesizeF}` , 'conversation', {citado: m, thumbnail: global.thumb2, información_de_contexto:{externalAdReply: {titulo: 'Simple WhatsApp bot', body: `© ${conn.user.name}`, URL_de_origen: '', thumbnail: y}}})
+  if (!isLimit) conn.sendFile(m.chat, dl_link , 'conversation', {quoted: m, thumbnail: y, contextInfo:{externalAdReply: {titulo: `${title}`, body: `© ${conn.user.name}`, sourceUrl: '', thumbnail: y}}}, m, false, {ptt: true, duration: 999999999999})
 }
 handler.command = /^dlmsc$/i
 handler.owner = false
