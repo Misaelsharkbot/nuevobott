b86d03d6913ffba39b466f3eefadc9c8d9eefe0f
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-14 16:29:21 -0500

    Update caklontong_answer.js

--- plugins/caklontong_answer.js
+++ plugins/caklontong_answer.js
@@ -1,19 +1,19 @@
 let handler = m => m
 handler.before = async function (m) {
   let id = m.chat
-  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*hint caklontong/i.test(m.quoted.text)) return
+  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Tipo.*Sugerencia caklontong/i.test(m.quoted.text)) return
   conn.caklontong = conn.caklontong ? conn.caklontong : {}
-  if (!(id in conn.caklontong)) throw 'Soal itu telah berakhir!'
+  if (!(id in conn.caklontong)) throw '¡Se acabó esa pregunta!'
   if (m.quoted.id == conn.caklontong[id][0].id) {
     let json = JSON.parse(JSON.stringify(conn.caklontong[id][1]))
     // m.reply(JSON.stringify(json, null, '\t'))
     if (m.text.toLowerCase() == json.result.jawaban.toLowerCase()) {
       global.DATABASE._data.users[m.sender].exp += conn.caklontong[id][2]
-      m.reply(`*Benar!*\n+${conn.caklontong[id][2]} XP`)
+      m.reply(`*¡Verdadero!*\n+${conn.caklontong[id][2]} XP`)
       clearTimeout(conn.caklontong[id][3])
       delete conn.caklontong[id]
-    } else if (m.text.toLowerCase().endsWith(json.result.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
-    else m.reply(`*Salah!*`)
+    } else if (m.text.toLowerCase().endsWith(json.result.jawaban.split` `[1])) m.reply(`*¡Un poco más!*`)
+    else m.reply(`*¡Equivocado!*`)
   }
 }
 handler.exp = 0
