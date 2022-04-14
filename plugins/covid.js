11ccaf3706db4d4133160caf08a2616e4d679787
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-14 17:04:56 -0500

    Update covid.js

--- plugins/covid.js
+++ plugins/covid.js
@@ -3,13 +3,13 @@ let handler = async (m, { text }) => {
   let res = await fetch(global.API('https://covid19.mathdro.id', '/api/countries/'+ (text)))
   if (!res.ok) throw await res.text()
   let json = await res.json()
-  if (!json.confirmed) throw 'Negara?'
+  if (!json.confirmed) throw '¿País?'
   if (json.confirmed) m.reply(`
-Countries : ${text}
-Confirmed : ${json.confirmed.value}
-Recovered : ${json.recovered.value}
-Deaths : ${json.deaths.value}
-Last Update : ${json.lastUpdate}
+Los paises : ${text}
+Confirmados : ${json.confirmed.value}
+Recuperados : ${json.recovered.value}
+Fallecidos : ${json.deaths.value}
+Última actualización : ${json.lastUpdate}
 \n\n2021 © Mythia Batford`.trim())
   else throw json
 }
