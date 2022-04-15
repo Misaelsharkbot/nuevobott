96c050987494164e30beb7b18cf45219eec4ab3c
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-15 15:47:09 -0500

    Update tourl.js

--- plugins/tourl.js
+++ plugins/tourl.js
@@ -4,13 +4,13 @@ const uploadImage = require('../lib/uploadImage')
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw 'No media found'
+  if (!mime) throw 'No se encontraron medios'
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
   m.reply(`${link}
 ${media.length} Byte(s)
-${isTele ? '(No Expiry Date)' : '(1 Time Use + Expired in 1 Week)'}`)
+${isTele ? '(Sin fecha de caducidad)' : '(1 Time Use + Expired in 1 Week)'}`)
 }
 handler.help = ['tourl (caption|reply media)']
 handler.tags = ['tools']
