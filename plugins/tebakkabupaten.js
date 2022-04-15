e7296356de4f7cc90a68b214f43b1a76c8bcb0e7
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-15 16:06:05 -0500

    Update tebakkabupaten.js

--- plugins/tebakkabupaten.js
+++ plugins/tebakkabupaten.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     conn.tebakkabupaten = conn.tebakkabupaten ? conn.tebakkabupaten : {}
     let id = m.chat
     if (id in conn.tebakkabupaten) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakkabupaten[id][0])
+        conn.reply(m.chat, 'Todavía hay preguntas sin respuesta en este chat.', conn.tebakkabupaten[id][0])
         throw false
     }
     let res = await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tebakkabupaten.json')
@@ -13,7 +13,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     let data = await res.json()
     let json = data[Math.floor(Math.random() * data.length)]
     let caption = `
-Timeout *${(timeout / 1000).toFixed(2)} detik*
+Timeout *${(timeout / 1000).toFixed(2)} segundos*
 Ketik ${usedPrefix}tekb untuk bantuan
 Bonus: ${poin} XP
 `.trim()
@@ -21,7 +21,7 @@ Bonus: ${poin} XP
         await conn.sendButtonImg(m.chat, await (await fetch(json.url)).buffer(), caption, 'MYTHIA×YUTA', 'Bantuan', '.ineedhelp', m),
         json, poin,
         setTimeout(async () => {
-            if (conn.tebakkabupaten[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.title}*`, 'MYTHIA×YUTA', 'Tebak Kabupaten', '.tebakkabupaten', conn.tebakkabupaten[id][0])
+            if (conn.tebakkabupaten[id]) await conn.sendButton(m.chat, `¡Se acabó el tiempo!\nLa respuesta era *${json.title}*`, 'MYTHIA×YUTA', 'Tebak Kabupaten', '.tebakkabupaten', conn.tebakkabupaten[id][0])
             delete conn.tebakkabupaten[id]
         }, timeout)
     ]
