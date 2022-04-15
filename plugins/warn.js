a843b2a650e1d97314e8bf7752f7f671610d8ed0
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-15 14:01:37 -0500

    Update warn.js

--- plugins/warn.js
+++ plugins/warn.js
@@ -1,21 +1,21 @@
 let handler = async (m, { conn, args }) => {
-    if (!args || !args[0]) throw 'Siapa yang mau di Warn om?'
+    if (!args || !args[0]) throw '¿A quien quiere advertir?'
     let mention = m.mentionedJid[0] || conn.parseMention(args[0]) || (args[0].replace(/[@.+-]/g, '').replace(' ', '') + '@s.whatsapp.net') || ''
-    if (!mention) throw 'Tag salah satu lah'
-    if (!(mention in global.DATABASE._data.users)) throw 'User tidak terdaftar dalam DATABASE!!'
+    if (!mention) throw 'Etiqueta uno'
+    if (!(mention in global.DATABASE._data.users)) throw '¡¡Usuario no está registrado en BASE DE DATOS!!'
     let user = global.DATABASE._data.users[mention]
-    if (user.Banneduser) throw 'User telah terbanned!!'
+    if (user.Banneduser) throw '¡El usuario ha sido baneado!'
     if ((user.warn * 1) < 3) {
         user.warn += 1
-        m.reply('berhasil Warn')
-        m.reply('Kamu di warn oleh OWNER Atau MODERATOR!!, dan sekarang kamu punya *' + (user.warn + 1) + '* WARN. Ingat Jika kamu mendapat warn 4 kali kamu akan otomatis ke banned', mention)
+        m.reply('LISTO')
+        m.reply('Kamu di warn oleh OWNER Atau MODERATOR!!, y ahora tienes *' + (user.warn + 1) + '* ADVERTENCIA. Recuerda que si te avisan 4 veces serás baneado automáticamente', mention)
     } else if ((user.warn * 1) > 2) {
-        let reason = (args.length > 0 || args[1] ? args.slice(1).join(' ') : '4 kali WARN') || '4 kali WARN'
+        let reason = (args.length > 0 || args[1] ? args.slice(1).join(' ') : '4 kali WARN') || '4 veces ADVERTENCIA'
         user.Banneduser = true
         user.BannedReason = reason
         user.warn = 0
-        m.reply('*Dia sudah Terbanned, karena mendapatkan 4 warn*')
-        m.reply('*Kamu Terbanned karena telah mendapatkan 4 kali warn*\n *HUBUNGI* \n' + global.owner.map((v, i) => '*Owner ' + (i + 1) + ':* wa.me/' + v).join('\n') + '\n\n' + global.mods.map((v, i) => '*Moderator ' + (i + 1) + ':* wa.me/' + v).join('\n'), mention)
+        m.reply('*Ha sido baneado, por recibir 4 advertencias*')
+        m.reply('*Estás baneado por recibir 4 advertencias*\n *HUBUNGI* \n' + global.owner.map((v, i) => '*Owner ' + (i + 1) + ':* wa.me/' + v).join('\n') + '\n\n' + global.mods.map((v, i) => '*Moderator ' + (i + 1) + ':* wa.me/' + v).join('\n'), mention)
     }
 }
 
