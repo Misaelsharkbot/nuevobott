f4a6f2451e8cd359d47dd39f73bc0be73d3faef7
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-15 16:28:50 -0500

    Update menuutama.js

--- plugins/menuutama.js
+++ plugins/menuutama.js
@@ -10,7 +10,7 @@ let res = 'https://telegra.ph/file/939d32f2758468c27ea56.mp4'
 let jam = new Date
 let clock = jam.toLocaleTimeString('us')
 let waktu = new Date().getHours()
-let ucapWaktu = waktu > 2 && waktu < 4 ? 'Dini Hari' : waktu < 11 ? 'ðŸŒ„ Good Morning' : waktu < 16 ? 'â˜€ï¸Good Afternoon' : waktu < 19 ? 'ðŸŒ‡ Good Afternoon' : 'ðŸŒ™ Good Night'
+let ucapWaktu = waktu > 2 && waktu < 4 ? 'Primeros ' : waktu < 11 ? 'ðŸŒ„ Buenos días' : waktu < 16 ? 'â˜€ï¸Buena tarde' : waktu < 19 ? 'ðŸŒ‡ Good Afternoon' : 'ðŸŒ™ Buenas noches'
 let pp = 'https://telegra.ph/file/a2ae6cbfa40f6eeea0cf1.jpg'
   try {
     pp = await conn.getProfilePicture(m.sender)
@@ -21,7 +21,7 @@ let pp = 'https://telegra.ph/file/a2ae6cbfa40f6eeea0cf1.jpg'
 let anu = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
  //*****************MENU*********************
  conn.send3ButtonVid(m.chat, res, `*${ucapWaktu} ${conn.getName(m.sender)}*`, `
-Saya Adalah Bot ${conn.getName(conn.user.jid)} Yg Di Kembangkan Oleh @${global.kontak[0].split`@`[0]} Silahkan Tekan Tombol Button "MENU" Untuk Melihat List Command Dari Bot ${conn.getName(conn.user.jid)} Ketik .listmenu
+soy un bot me llamo ${conn.getName(conn.user.jid)}  y fui desarrollado por @${global.kontak[0].split`@`[0]} Por favor presione el botón "MENU" Para ver la lista de comandos del bot ${conn.getName(conn.user.jid)} Ketik .listmenu
 Support By doujindesu.com
 Powered By @${global.kontak[0].split`@`[0]}`.trim(), 'LIST MENU', '.listmenu', 'OWNER', '.owner', 'THANKS TO', 'tqto', 'conversation', { quoted: anu, thumbnail: global.image1,contextInfo: { mentionedJid: ['6289637133848@s.whatsapp.net'], forwardingScore: 999, isForwarded: true, externalAdReply: { title: `${ucapWaktu}`, body: `Time: ${clock}\nRuntime: ${Rtime}`, mediaUrl: 'https://www.facebook.com/Inunime-107082474576049/', mediaType: 2, thumbnail: await (await fetch(pp)).buffer()}}})
 }
