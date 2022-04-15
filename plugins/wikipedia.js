5cb56af9cd5e54e0a965729d49322b37bfa72d02
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-15 13:44:01 -0500

    Update wikipedia.js

--- plugins/wikipedia.js
+++ plugins/wikipedia.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { args }) => {
-  if (!args) return m.reply('Mau cari apa?')
+  if (!args) return m.reply('¿Qué estás buscando?')
   let res = await fetch(`https://zahirr-web.herokuapp.com/api/wikipedia?search=${args[0]}&apikey=zahirgans`)
   let json = await res.json()
   if (json.result.status_code) m.reply(json.result.message + '!!')
-  else m.reply('Menurut wikipedia *' + json.result.result + '*')
+  else m.reply('segun wikipedia *' + json.result.result + '*')
 }
 handler.help = ['wikipedia']
 handler.tags = ['internet']
