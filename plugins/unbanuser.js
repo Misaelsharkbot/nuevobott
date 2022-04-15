24e6fb79244616c35b232a2788f5b357371cccaa
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-15 15:17:39 -0500

    Update unbanuser.js

--- plugins/unbanuser.js
+++ plugins/unbanuser.js
@@ -2,14 +2,14 @@ let handler = async (m, { conn, args }) => {
     if (!args || !args[0]) throw 'Yang mau di Unban siapa?'
     let mention = m.mentionedJid[0] || conn.parseMention(args[0]) || (args[0].replace(/[@.+-]/g, '').replace(' ', '') + '@s.whatsapp.net') || ''
     if (!mention) throw 'Tag salah satu lah'
-    if (!(mention in global.DATABASE._data.users)) throw 'User tidak terdaftar dalam DATABASE!!'
+    if (!(mention in global.DATABASE._data.users)) throw '¡¡Usuario no está registrado en BASE DE DATOS!!'
     let user = global.DATABASE._data.users[mention]
-    if (!user.Banneduser) throw 'User tidak Terbanned!!'
+    if (!user.Banneduser) throw '¡¡Usuario no baneado!!'
     user.Banneduser = false
     user.BannedReason = ''
     user.warn = 0
-    await m.reply('Berhasil unbanned!!')
-    m.reply('Kamu telah di Unbanned!!', mention)
+    await m.reply('¡¡Desbaneado con éxito!!')
+    m.reply('Has sido desbaneado!!', mention)
 }
 handler.help = ['unban']
 handler.tags = ['owner']
