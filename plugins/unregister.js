43fa5c1104faaea5ea78d9aea4fe1a563e0787b8
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-15 15:14:52 -0500

    Update unregister.js

--- plugins/unregister.js
+++ plugins/unregister.js
@@ -1,13 +1,13 @@
 const { createHash } = require('crypto')
 let handler = async function (m, { args }) {
-  if (!args[0]) throw 'Serial Number kosong'
+  if (!args[0]) throw 'Número de serie vacío'
   let user = global.DATABASE._data.users[m.sender]
   let sn = createHash('md5').update(m.sender).digest('hex')
-  if (args[0] !== sn) throw 'Serial Number salah'
+  if (args[0] !== sn) throw 'Número de serie incorrecto'
   user.registered = false
-  m.reply(`Unreg berhasil!`)
+  m.reply(`¡Unreg lo hizo!`)
 }
-handler.help = ['', 'ister'].map(v => 'unreg' + v + ' <SN|SERIAL NUMBER>')
+handler.help = ['', 'ister'].map(v => 'unreg' + v + ' <SN|NUMERO DE SERIE>')
 handler.tags = ['exp']
 
 handler.command = /^unreg(ister)?$/i
