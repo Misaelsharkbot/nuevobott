fb4d4d3adea0762e53273bdabc9b8bd055d1827d
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-15 16:15:56 -0500

    Update scan.js

--- plugins/scan.js
+++ plugins/scan.js
@@ -2,8 +2,8 @@
 // https://github.com/TOXIC-DEVIL
 
 let handler = async (m, { conn, args }) => {
-    if (!args || !args[0] || args.length === 0) throw 'Masukkan nomor untuk dipindai!'
-    if (args[0].startsWith('0')) throw 'Gunakan code Negara!'
+    if (!args || !args[0] || args.length === 0) throw 'Ingrese el número para escanear!'
+    if (args[0].startsWith('0')) throw '¡Utilice el código de país!'
     let user = await conn.isOnWhatsApp(args[0])
     let exists = user && user.exists ? true : false
     if (exists) {
@@ -15,21 +15,21 @@ let handler = async (m, { conn, args }) => {
         }
         if (user.jid in global.DATABASE._data.users) isInDatabase = true
         let str = ` 
-*Nama:* ${conn.getName(user.jid)}
-*Nomor:* ${splitM(user.jid)}
-*Mention:* ${toM(user.jid)}
+*Nombre:* ${conn.getName(user.jid)}
+*Numero:* ${splitM(user.jid)}
+*Mentnio:* ${toM(user.jid)}
 *Api:* wa.me/${splitM(user.jid)}
 *Jid:* ${user.jid}
 *Whatsapp Bussines:* ${user.isBusiness ? 'Yes' : 'No'}
-*In Database:* ${isInDatabase ? 'Yes' : 'No'}
-*Group Yang Sama Dengan BOT:* ${sameGroup.length} *Group*
+*Database:* ${isInDatabase ? 'Yes' : 'No'}
+*Grupos con el bot:* ${sameGroup.length} *Grupos*
 `.trim()
         m.reply(str, m.chat, { 
             contextInfo: { 
                 mentionedJid: conn.parseMention(str)
             }
         })
-    } else throw 'User Tidak Ditemukan!!'
+    } else throw '¡¡Usuario no encontrado!!'
 }
     
 handler.help = ['scan'].map(v => v + ' [nomor]')
