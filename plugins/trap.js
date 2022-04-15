b69a37ea3e9bbead2711808441c488bf9ad96463
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-15 15:31:24 -0500

    Update trap.js

--- plugins/trap.js
+++ plugins/trap.js
@@ -6,7 +6,7 @@ let handler = async(m, { conn }) => {
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'Ciee Kena Tipu:V', m)
+  conn.sendFile(m.chat, json.url, '', 'Noooo fui engañado :V', m)
 }
 handler.help = ['trap']
 handler.tags = ['dewasa']
