875de220b3250f2305f838d6b399db3f0438e321
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-15 15:05:27 -0500

    Update use.js

--- plugins/use.js
+++ plugins/use.js
@@ -2,15 +2,15 @@ let { MessageType } = require('@adiwajshing/baileys')
 let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) =>  {
     let msgerror = (pickRandom(['Error', 'astagfirullah error', 'Nice Error', 'Salah format keknya :v', 'error bro', 'Kocak error :v', 'wtf error :v', 'Ciaaa error', 'error cuyy', 'dahlah (emot batu) error']))
     try {
-        let msgkurang = (pickRandom(['potionmu tidak cukup', 'ciaa gk cukup potionyya :v', 'wtf gk cukup :v', 'beli potion dulu, potionmu gk cukup', 'Duaarr potionmu gk cukup', 'eyyyy potionmu kurang', 'beli dulu lah, masak mau pakai potion tapi gk ada potionnnya :v', 'minta ke orang lain suruh transfer potion, biar potionmu gk kurang :v', 'Beli potion dulu KK']))
-        let msgpenuh = (pickRandom(['Nyawamu sudah penuh', 'coba deh liat inv mu, nyawamu kan dah 100 ngapai ngunain potion lagi?', 'health mu dah penuh woyy', 'ws kebek weh :v', 'nyawamu dah penuh :v', 'udh weh, udh penuh']))
+        let msgkurang = (pickRandom(['no tienes suficientes pociones', 'ups, no hay suficientes pociones :v', 'wtf no es suficiente :v', 'compra pociones primero, no tienes suficientes pociones', 'No tienes suficientes pociones', 'eyyyy no tienes suficientes pociones', 'compra primero', 'cuando quieres usar pociones pero no hay pociones :v', 'pídele a otra persona que transfiera las pociones , para que no pierdas tus pociones :v', 'Compra pociones primero KK']))
+        let msgpenuh = (pickRandom(['Tu vida está llena', 'Echa un vistazo a tu inv, tu vida ya es 100, ¿para qué usar pociones otra vez?', 'Tu salud está llena woyy', 'ws kebek weh :v', 'tu vida es full :v', 'uhh wow, ya full']))
         let kucing = global.DATABASE._data.users[m.sender].kucing
         let usepotion = (kucing == 0 ? 40 : '' || kucing == 1 ? 45 : '' || kucing == 2 ? 50 : '' || kucing == 3 ? 55 : '' || kucing == 4 ? 60 : '' || kucing == 5 ? 65 : '' || kucing == 6 ? 70 : '' || kucing == 7 ? 75 : '' || kucing == 8 ? 80 : '' || kucing == 9 ? 85 : '' || kucing == 10 ? 90 : '')
         let healt = global.DATABASE._data.users[m.sender].healt
         if (/use|pakai/i.test(command)) {
             try {
                 let count = (/[0-9]/g.test(args[1])) ? !args[1] || args.length < 2 ? Math.max((Math.ceil((100 - global.DATABASE._data.users[m.sender].healt) / usepotion)), 1) : Math.max(args[1], 1) : Math.max((Math.ceil((100 - global.DATABASE._data.users[m.sender].healt) / usepotion)), 1)
-                 let msgsucces = (pickRandom(['success memakai', 'Nice succes menggunakan', 'berhasil meminum ', 'primitif anda menggunakan', 'anda memakai', 'Anda menggunakan']) + ' *' + (count * 1) + '* Potion')
+                 let msgsucces = (pickRandom(['success memakai', 'Buen éxito usando', 'se las arregló para beber', 'primitivo que usas', 'anda memakai', 'Anda menggunakan']) + ' *' + (count * 1) + '* Potion')
                  if (args[0] === 'potion') {
                     if (global.DATABASE._data.users[m.sender].healt < 100) {
                         if (global.DATABASE._data.users[m.sender].potion >= count * 1) {
@@ -19,7 +19,7 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) =>  {
                             conn.reply(m.chat, msgsucces, m)
                         } else conn.reply(m.chat, msgkurang, m)
                     } else conn.reply(m.chat, msgpenuh, m)
-                } else if (args.length > 2 && args[0] === !'potion') m.reply(pickRandom(['Hanya bisa menggunakan potion', 'Mau ngunain apa? Cuma bisa gunain potion :v', 'Wih mau gunain apa kamu, kan hanya bisa potion', 'Waduheck, hanya bisa potion', 'lah, mau gunain apa?, kan hanya bisa potion']) + '\nContoh penggunaan: *' + usedPrefix + 'potion 1*')
+                } else if (args.length > 2 && args[0] === !'potion') m.reply(pickRandom(['Solo puede usar pociones.', '¿Qué es lo que quieres hacer? Solo puedo usar pociones :v', 'Vaya, ¿qué vas a usar?, solo pueden pociones', 'Wow, solo puedes hacer pociones.', 'Bueno, ¿qué quieres usar?, solo puedes hacer pociones']) + '\nEjemplos de uso: *' + usedPrefix + 'potion 1*')
             } catch (e) {
                 console.log(e)
                 m.reply(msgerror)
@@ -33,7 +33,7 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) =>  {
         } else if (/heal/i.test(command)) {
             try {
                 let count = (/[0-9]/g.test(args[0])) ? !args[0] || args.length < 1 ? Math.max((Math.ceil((100 - global.DATABASE._data.users[m.sender].healt) / usepotion)), 1) : Math.max(args[0], 1) : Math.max((Math.ceil((100 - global.DATABASE._data.users[m.sender].healt) / usepotion)), 1)
-                let msgsucces = (pickRandom(['success memakai', 'Nice succes menggunakan', 'berhasil meminum ', 'primitif anda menggunakan', 'anda memakai', 'Anda menggunakan']) + ' *' + (count * 1) + '* Potion')
+                let msgsucces = (pickRandom(['éxito usando', 'buen éxito usando', 'beber con éxito', 'primitivo que usaste', 'usabas', 'usabas']) + ' *' + (count * 1) + '* Potion')
                 if (global.DATABASE._data.users[m.sender].healt < 100) {
                     if (global.DATABASE._data.users[m.sender].potion >= count * 1) {
                         global.DATABASE._data.users[m.sender].potion -= count * 1
